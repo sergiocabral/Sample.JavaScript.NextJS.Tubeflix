e739222211026026274acf817e0ee7d5c24c9ac8
Sergio Cabral <git@sergiocabral.com>
2024-11-30 02:03:19 -0300

    Navegando por rotas dinamicamente (useRouter)
    - Botão 'Estou com sorte'
    - useRouter de next/navigation
    - Precisa usar preventDefault()

--- src/components/HighlightVideo.js
+++ src/components/HighlightVideo.js
@@ -1,20 +1,29 @@
 'use client'
 
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import styles from "./HighlightVideo.module.css"
 import { getRandomItem, getVideos } from "@/helpers/helpers.js"
 import Link from "next/link.js"
+import { useRouter } from "next/navigation.js"
 
 export default function HighlightVideo({ tag }) {
   const [ video, setVideo ] = useState(null)
+  const videosList = useRef(null)
+  const router = useRouter()
 
   useEffect(() => {
     async function fetchVideos() {
-      const videosList = await getVideos(tag)
-      setVideo(getRandomItem(videosList))
+      videosList.current = await getVideos(tag)
+      setVideo(getRandomItem(videosList.current))
     }
     fetchVideos()
   }, [])
+
+  function onLuckyClick(event) {
+    const video = getRandomItem(videosList.current)
+    router.push(`/player/${video.key}`)
+    event.preventDefault()
+  }
 
   return (
     video && <div className={styles.video}>
@@ -23,6 +32,7 @@ export default function HighlightVideo({ tag }) {
         <h2 className={styles.title}>{video.title}</h2>
         <p className={styles.description}>{video.description}</p>
         <Link href={`/player/${video.key}`} className={styles.play}>Assistir</Link>
+        <Link href='#' onClick={onLuckyClick} className={styles.lucky}>Estou com sorte</Link>
       </div>
     </div>
   );
