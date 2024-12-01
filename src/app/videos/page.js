94c259dd8917f9536209d655949539b1673c8ff3
Sergio Cabral <git@sergiocabral.com>
2024-12-01 17:18:47 -0300

    Gerenciando o Título da Página em Componentes Server-Side
    - em /videos ->  export const metadata = { title: "Home" };
    - em /musics -> export function generateMetadata() ...

--- src/app/videos/page.js
+++ src/app/videos/page.js
@@ -2,11 +2,17 @@ import HighlightVideo from '@/components/HighlightVideo.js';
 import styles from './page.module.css'
 import VideoTags from '@/components/VideoTags.js';
 
+const tag = 'Vídeos'
+
+export const metadata = {
+  title: `Tubeflix - ${tag}`,
+}
+
 export default function VideosPage() {
   return (
     <>
-      <HighlightVideo tag="Vídeos" />
-      <VideoTags tag="Vídeos" />
+      <HighlightVideo tag={tag} />
+      <VideoTags tag={tag} />
     </>
   )
 }
