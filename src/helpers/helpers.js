cb639310709cb6ebf2fd1f433db7a71243e3c5d6
Sergio Cabral <git@sergiocabral.com>
2024-12-01 16:04:15 -0300

    Criando Endpoints com API Routes
    - Escrever /app/api/xyz/route.js com export async function GET() { return new NextResponse("Endpoint de API") }
    - Duplicar database.json em /src/data/videos.json
    - Implementar fs.readFile com process.cwd()
    - Trocar chamada para a api
    - Usar POST
    - Apagar database.json

--- src/helpers/helpers.js
+++ src/helpers/helpers.js
@@ -18,7 +18,7 @@ export function removeDuplicates(array) {
 }
 
 export async function getVideos(tag) {
-  const response = await fetch('/database.json', { next: { revalidate: 60 }})
+  const response = await fetch('/api/videos', { method: 'POST', next: { revalidate: 60 }})
   const videos = await response.json();
   return objectToArray(videos).filter(video => !tag || video.tags.includes(tag))
 }
