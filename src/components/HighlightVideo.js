2fe64aec6e67029a1ff3a38a3bd705cd61634016
Sergio Cabral <git@sergiocabral.com>
2024-12-05 20:00:00 -0300

    Carregando banco de videos

--- src/components/HighlightVideo.js
+++ src/components/HighlightVideo.js
@@ -5,36 +5,22 @@ import style from './HighlightVideo.module.css'
 import { getRandomItem, objectToArray } from '@/helpers/helpers'
 import Link from 'next/link'
 
-const videos = {
-    "BqXmSzyl67s": {
-        "title": "O Mistério da Origem da Vida",
-        "description": "Descubra os mistérios por trás do surgimento da vida no planeta Terra. Este documentário explora teorias científicas e ideias intrigantes que desafiam nossa compreensão do universo. Uma jornada emocionante pelo desconhecido.",
-        "tags": ["Vídeos", "Documentários e Informativos"]
-      },
-      "dwOlNBOdWZM": {
-        "title": "Troféu da Tríplice Coroa",
-        "description": "Acompanhe a emocionante busca por um troféu raro no universo dos leilões. Descubra a história e os desafios envolvidos para obter essa peça única e cheia de simbolismo.",
-        "tags": ["Vídeos", "Documentários e Informativos"]
-      },
-      "gcB4ay_fO1k": {
-        "title": "Cassete Lofi Nostálgico",
-        "description": "Uma playlist lofi com vibes de fita cassete. Sons retrô para momentos de estudo ou reflexão.",
-        "tags": ["Músicas", "Lo-Fi Relaxantes"]
-      },
-      "fzw_sQIvTbs": {
-        "title": "Lofi Outono",
-        "description": "Entre no clima do outono com este mix lofi aconchegante. Perfeito para relaxar em dias tranquilos.",
-        "tags": ["Músicas", "Lo-Fi Relaxantes"]
-      }  
-}
-
 export default function HighlightVideo({ tag }) {
     const [ video, setVideo ] = useState(null)
 
     useEffect(() => {
-        const videosList = objectToArray(videos)
-            .filter(video => tag == undefined || video.tags.includes(tag))
-        setVideo(getRandomItem(videosList))
+        async function fetchVideos() {
+            const response = await fetch('/database.json', {
+                next: {
+                    revalidate: 60
+                }
+            })
+            const videos = await response.json()
+            const videosList = objectToArray(videos)
+                .filter(video => tag == undefined || video.tags.includes(tag))
+            setVideo(getRandomItem(videosList))
+        }
+        fetchVideos()
     }, [])
 
     return (
