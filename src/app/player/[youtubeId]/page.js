5b264a3afd5c9a20d12294b8ee77c4ee66e29a2f
Sergio Cabral <git@sergiocabral.com>
2024-12-05 20:00:00 -0300

    Gerenciando o Título da Página em Componentes Client-Side
    - Definir título do Player

--- src/app/player/[youtubeId]/page.js
+++ src/app/player/[youtubeId]/page.js
@@ -3,9 +3,19 @@
 import YouTube from 'react-youtube'
 import styles from './page.module.css'
 import { useParams } from 'next/navigation.js'
+import { useEffect } from 'react';
 
 export default function PlayerPage() {
   const { youtubeId } = useParams();
+
+  useEffect(() => {
+    async function setTitle() {
+      const response = await fetch(`/api/video/${youtubeId}`, { method: 'POST', next: { revalidate: 60 } })
+      const video = await response.json()
+      document.title = `${video.title} - ${document.title}`
+    }
+    setTitle()
+  }, [])
 
   return (
     <div className={styles.player}>
