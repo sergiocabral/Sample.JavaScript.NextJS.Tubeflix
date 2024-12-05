d9b0b7928d6a877802a72ec58bd96af2e093f06b
Sergio Cabral <git@sergiocabral.com>
2024-12-05 20:00:00 -0300

    Estilos CSS como Módulos (*.module.css) #2

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import VideosPage from "./videos/page.js";
 export default function HomePage() {
   return (
     <>
-      <h2 className={styles.h2}>Inicio</h2>
+      <h2 className={styles.title}>Inicio</h2>
       <VideosPage />
       <MusicsPage />
     </>
