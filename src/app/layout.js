f0350b8756143d4355413e40536df7d105114e68
Sergio Cabral <git@sergiocabral.com>
2024-12-05 20:00:00 -0300

    Compartilhando Estado (useState) entre Componentes (useContext)
    - Duplicar <ThemeToggle /> e constatar que ficam inconsistentes
    - Criar ThemeContext e registrar em RootLayout
    - Mover useState para ThemeProvider

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Link from "next/link.js";
 import styles from "./layout.module.css";
 import ThemeToggle from "@/components/ThemeToggle.js";
+import { ThemeProvider } from "@/contexts/ThemeContext.js";
 
 export const metadata = {
   title: "Tubeflix",
@@ -11,23 +12,26 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt">
-      <body className={styles.body}>
-        <header className={styles.header}>
-          <h1><Link href="/">Tubeflix</Link></h1>
-          <nav>
-            <Link href="/videos">Vídeos</Link>
-            <Link href="/musics">Músicas</Link>
-          </nav>
-        </header>
-        <main className={styles.main}>
-          {children}
-        </main>
-        <footer className={styles.footer}>
-          <p>&copy; 2024 Tubeflix - Todos os direitos reservados</p>
-          <ThemeToggle />
-        </footer>
-      </body>
-    </html>
+    <ThemeProvider>
+      <html lang="pt">
+        <body className={styles.body}>
+          <header className={styles.header}>
+            <h1><Link href="/">Tubeflix</Link></h1>
+            <nav>
+              <Link href="/videos">Vídeos</Link>
+              <Link href="/musics">Músicas</Link>
+            </nav>
+          </header>
+          <main className={styles.main}>
+            {children}
+          </main>
+          <footer className={styles.footer}>
+            <p>&copy; 2024 Tubeflix - Todos os direitos reservados</p>
+            <ThemeToggle />
+            <ThemeToggle />
+          </footer>
+        </body>
+      </html>
+    </ThemeProvider>
   );
 }
