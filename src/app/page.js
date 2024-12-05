2b4baeda8729d422e90f09986a27dccdf7570db5
Sergio Cabral <git@sergiocabral.com>
2024-12-05 20:00:00 -0300

    Aplicado um CSS Reset

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,12 @@
+import MusicsPage from "./musics/page";
+import VideosPage from "./videos/page";
+
 export default function Home() {
   return (
-    <h2>Início</h2>
+    <>
+      <h2>Início</h2>
+      <VideosPage />
+      <MusicsPage />
+    </>    
   );
 }
